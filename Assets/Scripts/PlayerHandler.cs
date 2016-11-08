18cdaf251b1adf8cbb33383b03368617ce2326f9
CroSparks <jvyaris@gmail.com>
2016-11-07 23:28:54 -0800

    [Player2][Player1][Player scripts]
    [player2]
    - Added player 2 prefab
    - Added player 2 animations
    [player1]
    - just re-organized the hierarchy
    [playerhandler/playercontroller]
    - added stopcoroutine before all startcoroutines
    - added public gameobj "myModel" to get the animator component

--- Assets/Scripts/PlayerHandler.cs
+++ Assets/Scripts/PlayerHandler.cs
@@ -34,6 +34,7 @@ public class PlayerHandler : MonoBehaviour
 
     //Gameobjects
     private GameObject gameManager;
+    public GameObject myModel;
 
     //Components
     private Rigidbody myRigidbody;
@@ -71,15 +72,17 @@ public class PlayerHandler : MonoBehaviour
         //init components
         myRigidbody = GetComponent<Rigidbody>();
         player = GetComponent<PlayerController>();
-        gameManager = GameObject.Find("Game_Manager");
-        myAnimator = GetComponent<Animator>();
+        myAnimator = myModel.GetComponent<Animator>();
 
+        //get gameobjects
+        gameManager = GameObject.Find("Game_Manager");
+        Debug.Log(gameManager);
         //init variables
         rewardCountdown = maxRewardCountdown;
 
         //sounds
-        collisionSFX = GameObject.Find("Player/CollisionSFX").GetComponents<AudioSource>();
-        dodgeAudioSources = GameObject.Find("Player/PlayerRewardSFX").GetComponents<AudioSource>();
+        collisionSFX = GameObject.Find("CollisionSFX").GetComponents<AudioSource>();
+        dodgeAudioSources = GameObject.Find("PlayerRewardSFX").GetComponents<AudioSource>();
         slopeDownsource = collisionSFX[0];
         slopeUpsource = collisionSFX[1];
         platformFast = collisionSFX[2];
@@ -181,6 +184,7 @@ public class PlayerHandler : MonoBehaviour
             {
                 //reset
                 rewardIsActive = false;
+                StopCoroutine(Obstacle_Slow());
                 StartCoroutine(Obstacle_Slow());
             }
         }
@@ -192,6 +196,7 @@ public class PlayerHandler : MonoBehaviour
             {
                 //reset
                 rewardIsActive = false;
+                StopCoroutine(Obstacle_Stun());
                 StartCoroutine(Obstacle_Stun());
                 Destroy(col.gameObject, 0.5f);
             }
@@ -303,6 +308,7 @@ public class PlayerHandler : MonoBehaviour
     {
         if (col.gameObject.tag == "Incline")
         {
+            StopCoroutine(NormGround());
             StartCoroutine(NormGround());
         }
     }
@@ -319,6 +325,7 @@ public class PlayerHandler : MonoBehaviour
             if (angle.z >= 5f && angle.z <= 45f && ParentAngle.y == 0f)
             {
                 slopeUpsource.Play();
+                StopCoroutine(SlowGround());
                 StartCoroutine(SlowGround());
 
             }
@@ -328,6 +335,7 @@ public class PlayerHandler : MonoBehaviour
             if (angle.z >= 5f && angle.z <= 45f)
             {
                 slopeDownsource.Play();
+                StopCoroutine(FastGround());
                 StartCoroutine(FastGround());
             }
         }
@@ -385,257 +393,3 @@ public class PlayerHandler : MonoBehaviour
 
     }
 }
-
-//private AudioSource[]       audioSources;
-//private AudioSource         dodgedOb;//john
-//private AudioSource         powerUpGet; //john mate! fuuuuuuuuuu
-//
-//
-//void Start()
-//{
-//    //init components
-//    myRigidbody     = GetComponent<Rigidbody>();
-//    player          = GetComponent<PlayerController>();
-//    audioSources    = GameObject.Find("PlayerRewardSFX").GetComponents<AudioSource>();
-//
-//    //init audio clips
-//    dodgedOb        = audioSources[0];
-//    powerUpGet      = audioSources[1];
-//
-//
-//
-//    //init variables
-//    rewardCountdown = maxRewardCountdown;
-//
-//    //sounds
-//    slopeDownsource = GetComponent<AudioSource>();
-//
-//}
-//
-////Wall collide detection
-//void FixedUpdate()
-//{
-//    // Get the velocity
-//    Vector3 horizontalMove = myRigidbody.velocity;
-//    // Don't use the vertical velocity
-//    horizontalMove.y = 0;
-//    // Calculate the approximate distance that will be traversed
-//    float distance = horizontalMove.magnitude * Time.fixedDeltaTime;
-//    // Normalize horizontalMove since it should be used to indicate direction
-//    horizontalMove.Normalize();
-//    RaycastHit hit;
-//
-//    // Check if the body's current velocity will result in a collision
-//    if (myRigidbody.SweepTest(horizontalMove, out hit, distance) && hit.transform.gameObject.tag == "Wall")
-//    {
-//        // If so, stop the movement
-//        myRigidbody.velocity = new Vector3(0, myRigidbody.velocity.y, 0);
-//    }
-//
-//
-//}
-//
-//void Update()
-//{
-//    CheckPowerUpReward();
-//}
-//
-//void CheckPowerUpReward()
-//{
-//    if (rewardIsActive)
-//    {
-//        rewardCountdown -= Time.deltaTime;
-//        if (rewardCountdown < 0)
-//        {
-//            //Reset
-//            rewardIsActive = false;
-//        }
-//    }
-//    else
-//    {
-//        curAvoids = 0;
-//        rewardCountdown = maxRewardCountdown;
-//    }
-//}
-//
-//void OnTriggerEnter(Collider col)
-//{
-//    //OBSTACLES////////////////////////////////////
-//    //Slow
-//    if (col.gameObject.tag == "obSlow")
-//    {
-//        if (!isShielded)
-//        {
-//            //reset
-//            rewardIsActive = false;
-//            StartCoroutine(Obstacle_Slow());
-//        }
-//    }
-//
-//    //Stun
-//    if (col.gameObject.tag == "obStun")
-//    {
-//        if (!isShielded)
-//        {
-//            //reset
-//            rewardIsActive = false;
-//            StartCoroutine(Obstacle_Stun());
-//            Destroy(col.gameObject, 0.5f);
-//        }
-//        Destroy(col.gameObject);
-//    }
-//
-//    //Check if avoided obstacles
-//    if (col.gameObject.tag == "Avoided")
-//    {
-//        //Check if reward timer is not active
-//        if (!rewardIsActive && myPowerUp == PowerUp_State.None)
-//        {
-//            rewardIsActive = true;
-//        }
-//
-//        //Check if reward timer is active
-//        if (rewardIsActive)
-//        {
-//            curAvoids++;                //increment the number of avoided obstacles
-//            if (curAvoids >= maxAvoids) //if the number of avoided obstacles have past the max
-//            {
-//                myPowerUp = (PowerUp_State)Random.Range(1, 4);
-//                powerUpGet.Play();//play powerup get
-//                curAvoids = 0;
-//                Debug.Log("duude you got a powerup. NICE!");
-//
-//                //reset
-//                rewardIsActive = false;
-//            }
-//        }
-//
-//        if(curAvoids != maxAvoids - 1)
-//        {
-//          dodgedOb.Play();//play dodge ob
-//          Debug.Log("WHAT A DODGE");
-//        }
-//
-//    }
-//
-//    //platform(ground) slow and fast
-//    if (col.gameObject.tag == "Platformslow")
-//
-//    {
-//        if (player.movSpeed > 0f)
-//
-//        {
-//            Debug.Log("slow");
-//
-//
-//            player.movSpeed -= 1f;
-//        }
-//    }
-//
-//    if (col.gameObject.tag == "Platformreallyslow")
-//    {
-//        if (player.movSpeed > 0f)
-//
-//        {
-//            Debug.Log("slow");
-//
-//
-//            player.movSpeed -= 5f;
-//        }
-//    }
-//    if (col.gameObject.tag == "Platformfast")
-//    {
-//        if (player.movSpeed > 0f)
-//
-//        {
-//            Debug.Log("fast");
-//
-//
-//            player.movSpeed += 3f;
-//        }
-//    }
-//    if (col.gameObject.tag == "Platformreallyfast")
-//    {
-//        if (player.movSpeed > 0f)
-//
-//        {
-//            Debug.Log("fast");
-//
-//
-//            player.movSpeed += 5f;
-//        }
-//    }
-//}
-//
-////stay on platform
-//void OnCollisionStay(Collision col)
-//{
-//    if (col.gameObject.tag == "Platformmoving")
-//
-//    {
-//       
-//        if (timer <=2f)
-//        {
-//            this.transform.position = col.transform.position;
-//        }
-//    }
-//}
-//
-////Incline
-//void OnCollisionEnter(Collision col)
-//{
-//
-//    if (col.gameObject.tag == "Incline")
-//    {
-//        inclineslowfast(col.gameObject);
-//    }
-//}
-//
-//void inclineslowfast(GameObject incline)
-//{
-//    Vector3 angleInclie = incline.transform.eulerAngles;
-//    Debug.Log(angleInclie);
-//    if (angleInclie.z >= 45f && angleInclie.z <= 315f)
-//    {
-//        player.movSpeed -= 4f;
-//        //Debug.Log("slow");
-//        //Debug.Log(player.movSpeed);
-//    }
-//    else if (angleInclie.z >= 315f)
-//    {
-//        slopeDownsource.PlayOneShot(slopeDown, 0.5f);
-//
-//        player.movSpeed += 4f;
-//        //Debug.Log("fast");
-//        //Debug.Log(player.movSpeed);
-//    }
-//}
-//
-//void OnCollisionExit(Collision col)
-//{
-//    if (col.gameObject.tag == "Incline")
-//    {
-//        player.movSpeed = 15f;
-//
-//    }
-//}
-//
-////Obstacles
-//
-////Slow
-//IEnumerator Obstacle_Slow()
-//{
-//    float prevSpeed = player.movSpeed;
-//    player.movSpeed -= 5f;
-//    yield return new WaitForSeconds(5f);
-//    player.movSpeed = prevSpeed;
-//}
-//
-////Stun
-//IEnumerator Obstacle_Stun()
-//{
-//    float prevSpeed = player.movSpeed;
-//    player.movSpeed = 0f;
-//    yield return new WaitForSeconds(0.5f);
-//    player.movSpeed = prevSpeed;
-//}
