afebaef98fac30c9490eef0aca7e979fa41cbec8
wynne1204 <363424@AIV.EDMC.STU>
2016-11-08 14:22:22 -0800

    Add UI/SFX/BGM...

---
+++ Assets/Assets/Scripts/Altar.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Altar : MonoBehaviour
+{
+    public GameObject prefabPlayer;
+    public GameObject[] players;
+    private GameObject gameManager;
+
+    private int flag = 0;
+
+    void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        gameManager = GameObject.Find("Game_Manager");
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (players.Length == 2)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+
+                if (col.gameObject == players[0])
+                {
+                    players[0].GetComponent<PlayerHandler>().myRole = PlayerHandler.Role.Chaser;
+                    players[1].GetComponent<PlayerHandler>().myRole = PlayerHandler.Role.Runner;
+                    gameManager.GetComponent<Game_Manager>().isPhase2 = true;
+             
+                    flag = 1;
+                    //Destroy(this.gameObject);
+                }
+                if (col.gameObject == players[1])
+                {
+                    players[0].GetComponent<PlayerHandler>().myRole = PlayerHandler.Role.Runner;
+                    players[1].GetComponent<PlayerHandler>().myRole = PlayerHandler.Role.Chaser;
+                    gameManager.GetComponent<Game_Manager>().isPhase2 = true;
+
+                    flag = 2;
+                    //Destroy(this.gameObject);
+                }
+            }
+        }
+    }
+
+    public int Justflag()
+    {
+        return flag;
+    }
+}
